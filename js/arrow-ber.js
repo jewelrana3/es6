b715191329d60073dd1ebd9fdaf20c86c7ca9cb9
jewelrana3 <juyelrana7752@gmail.com>
2023-02-20 17:49:42 +0600

    menu

--- js/arrow-ber.js
+++ js/arrow-ber.js
@@ -1,6 +1,6 @@
 const add = (first,second) => first + second;
 const getElement = (first,last) => first + ' ' + last;
 const number = (a,b) => a*b;
-const numberSide = number(7,8)
+const numberSide = add(7*8)
 console.log(numberSide)
-// console.log('kfkf')
+console.log('kfkf')
