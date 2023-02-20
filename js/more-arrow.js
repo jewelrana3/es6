5d76e39c077edf7cc75ee4bf09345c17872109be
jewelrana3 <juyelrana7752@gmail.com>
2023-02-20 18:44:22 +0600

    larage

--- js/more-arrow.js
+++ js/more-arrow.js
@@ -2,5 +2,20 @@ const add = (first,second) => first + second;
 const getElement = (first,last) => first + ' ' + last;
 const number = (a,b) => a*b;
 const numberSide = number(7,8)
-console.log(numberSide)
+// console.log(numberSide)
 // console.log('kfkf')
+ const allOne = () => 3.23;
+ console.log(allOne)
+
+ const ponitAll = (num) => num *2;
+ const numberS = ponitAll(3);
+//  console.log(numberS)
+const addNumber = (a,b,c) => {
+    const first = a + b;
+    const second = b + c;
+    const allAdd = first * second;
+    const dividNumber = allAdd / 2;
+    console.log(dividNumber)
+    return dividNumber;
+}
+addNumber(2,3,4)
---
+++ js/spread.js
@@ -0,0 +1,2 @@
+const max = Math.max(33,23,243,545,33);
+console.log(max)
